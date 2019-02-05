0788616138e256e94f44b0b4af6ced93e7adfe95
Akuma Isaac Akuma <Akumanewton@gmail.com>
2019-02-05 16:29:15 +0100

    rebuild

--- index.d.ts
+++ index.d.ts
@@ -8,6 +8,7 @@ declare class IPC extends EventEmitter {
     init(arg?: string[]): this;
     kill(): void;
     send(eventType: string, data: any): void;
+    sendRaw(eventType: string, data: any, isSendAndReceive?: boolean): void;
     private _send;
     sendAndReceive(eventName: string, data: any, cb: (error: Error, data: any) => void): void;
 }
--- index.js
+++ index.js
@@ -1,101 +1,112 @@
-'use strict'
-Object.defineProperty(exports, '__esModule', { value: true })
-const events_1 = require('events')
-const child_process_1 = require('child_process')
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const events_1 = require("events");
+const child_process_1 = require("child_process");
 class IPC extends events_1.EventEmitter {
-  constructor(binPath) {
-    super()
-    this.binPath = binPath
-    this.go = null
-    this.closed = false
-  }
-  init(arg = []) {
-    this.closed = false
-    const self = this
-    const go = child_process_1.spawn(this.binPath, arg)
-    this.go = go
-    go.stderr.setEncoding('utf8')
-    go.stdout.setEncoding('utf8')
-    go.stderr.on('error', e => self.emit('log', e))
-    go.stderr.on('data', e => self.emit('log', e))
-    let outBuffer = ''
-    go.stdout.on('data', s => {
-      if (isJSON(s)) {
-        s.endsWith('}\\n') ? s.replace('}\\n', '}') : s
-        let payload = parseJSON(s)
-        if (typeof payload === 'object' && payload !== null) {
-          self.emit('data', payload)
-          let { error, data, event } = payload
-          self.emit(event, data, error)
-        }
-        return
-      }
-      outBuffer += s
-      if (s.endsWith('}\\n')) {
-        let d = outBuffer.replace('}\\n', '}')
-        let payload = parseJSON(d)
-        if (typeof payload === 'object' && payload !== null) {
-          self.emit('data', payload)
-          let { error, data, event } = payload
-          self.emit(event, data, error)
-        }
-        outBuffer = ''
-      }
-    })
-    go.once('close', _ => self.emit('close'))
-    return this
-  }
-  kill() {
-    this.closed = true
-    if (this.go) this.go.kill()
-  }
-  send(eventType, data) {
-    this._send(eventType, data, false)
-  }
-  _send(eventType, data, SR) {
-    if (!this.go || this.closed) return
-    if (this.go.killed) return
-    if (this.go && this.go.stdin) {
-      let payload
-      if (typeof data === 'object' || Array.isArray(data)) payload = JSON.stringify(data)
-      else payload = data
-      let d = JSON.stringify({
-        event: eventType,
-        data: payload,
-        SR: !!SR
-      })
-      if (this.go.stdin) {
-        this.go.stdin.write(d + '\n')
-      }
+    constructor(binPath) {
+        super();
+        this.binPath = binPath;
+        this.go = null;
+        this.closed = false;
     }
-  }
-  sendAndReceive(eventName, data, cb) {
-    this._send(eventName, data, true)
-    let rc = eventName + '___RC___'
-    this.on(rc, (data, error) => {
-      if (typeof cb === 'function') cb(error, data)
-    })
-  }
+    init(arg = []) {
+        this.closed = false;
+        const self = this;
+        const go = child_process_1.spawn(this.binPath, arg);
+        this.go = go;
+        go.stderr.setEncoding('utf8');
+        go.stdout.setEncoding('utf8');
+        go.stderr.on('error', e => self.emit('log', e));
+        go.stderr.on('data', e => self.emit('log', e));
+        let outBuffer = '';
+        go.stdout.on('data', s => {
+            if (isJSON(s)) {
+                s.endsWith('}\\n') ? s.replace('}\\n', '}') : s;
+                let payload = parseJSON(s);
+                if (typeof payload === 'object' && payload !== null) {
+                    self.emit('data', payload);
+                    let { error, data, event } = payload;
+                    self.emit(event, data, error);
+                }
+                return;
+            }
+            outBuffer += s;
+            if (s.endsWith('}\\n')) {
+                let d = outBuffer.replace('}\\n', '}');
+                let payload = parseJSON(d);
+                if (typeof payload === 'object' && payload !== null) {
+                    self.emit('data', payload);
+                    let { error, data, event } = payload;
+                    self.emit(event, data, error);
+                }
+                outBuffer = '';
+            }
+        });
+        go.once('close', _ => self.emit('close'));
+        return this;
+    }
+    kill() {
+        this.closed = true;
+        if (this.go)
+            this.go.kill();
+    }
+    send(eventType, data) {
+        this._send(eventType, data, false);
+    }
+    sendRaw(eventType, data, isSendAndReceive = false) {
+        this._send(eventType, data, isSendAndReceive);
+    }
+    _send(eventType, data, SR) {
+        if (!this.go || this.closed)
+            return;
+        if (this.go.killed)
+            return;
+        if (this.go && this.go.stdin) {
+            let payload;
+            if (typeof data === 'object' || Array.isArray(data))
+                payload = JSON.stringify(data);
+            else
+                payload = data;
+            let d = JSON.stringify({
+                event: eventType,
+                data: payload,
+                SR: !!SR
+            });
+            if (this.go.stdin) {
+                this.go.stdin.write(d + '\n');
+            }
+        }
+    }
+    sendAndReceive(eventName, data, cb) {
+        this._send(eventName, data, true);
+        let rc = eventName + '___RC___';
+        this.on(rc, (data, error) => {
+            if (typeof cb === 'function')
+                cb(error, data);
+        });
+    }
 }
 function parseJSON(s) {
-  try {
-    let data = s.replace(/}\\n/g, '}')
-    if (data.endsWith(',')) {
-      data = data.slice(0, -1)
+    try {
+        let data = s.replace(/}\\n/g, '}');
+        if (data.endsWith(',')) {
+            data = data.slice(0, -1);
+        }
+        let payload = JSON.parse(`[${data}]`);
+        return payload[0];
     }
-    let payload = JSON.parse(`[${data}]`)
-    return payload[0]
-  } catch (error) {
-    return null
-  }
+    catch (error) {
+        return null;
+    }
 }
 function isJSON(s) {
-  try {
-    s = s.endsWith('}\\n') ? s.replace('}\\n', '}') : s
-    JSON.parse(s)
-    return true
-  } catch (error) {
-    return false
-  }
+    try {
+        s = s.endsWith('}\\n') ? s.replace('}\\n', '}') : s;
+        JSON.parse(s);
+        return true;
+    }
+    catch (error) {
+        return false;
+    }
 }
-exports.default = IPC
+exports.default = IPC;
